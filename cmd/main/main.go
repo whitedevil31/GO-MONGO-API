2c26c15987602483cd95a855a1c1925426fbdfcd
whietedevil31 <magz3116@gmail.com>
2022-10-06 19:31:29 +0530

    Change default host

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 	"github.com/whitedevil31/go-mongo-api/pkg/routes"
 )
 
-func main(){
+func main() {
 	r := mux.NewRouter()
 	routes.RegisterStudentRoutes(r)
 	http.Handle("/", r)
-	
-	log.Fatal(http.ListenAndServe("localhost:5000", r))
+	viper.SetConfigFile(".env")
+	value, _ := viper.Get("PORT").(string)
+	host := "0.0.0.0" + ":" + value
+	fmt.Println("APP RUNNING ON " + host)
+	http.ListenAndServe(host, r)
 
 }
